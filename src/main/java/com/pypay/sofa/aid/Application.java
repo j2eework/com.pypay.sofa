f2d42d5c743481fea67a121b1c584137629c2eb1
j2eework <jiahan1024@163.com>
2020-07-12 10:51:58 +0800

    sofa commit

--- src/main/java/com/pypay/sofa/aid/Application.java
+++ src/main/java/com/pypay/sofa/aid/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author PyPay
+ * @author PyPay -- 1
  */
 @SpringBootApplication
 public class Application {
